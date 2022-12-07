f217713301427fb739595df328fac6b8dd4461ad
Karanvir Bath <ksbath@sfu.ca>
2022-12-06 19:46:55 -0800

    Fix reschedule activity launch

--- app/src/main/java/com/G3/kalendar/broadcasts/RescheduleBroadcastReceiver.kt
+++ app/src/main/java/com/G3/kalendar/broadcasts/RescheduleBroadcastReceiver.kt
@@ -7,9 +7,17 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
 import com.G3.kalendar.AlarmManagement
 import com.G3.kalendar.Globals
 import com.G3.kalendar.ViewKanbanTicket
+import com.G3.kalendar.database.story.StoryDao
+import com.G3.kalendar.database.story.StoryRepository
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class RescheduleBroadcastReceiver : BroadcastReceiver() {
 
@@ -38,8 +46,20 @@ class RescheduleBroadcastReceiver : BroadcastReceiver() {
         alarmManagement.cancelAlarm(time, storyId)
 
         val activityIntent = Intent(context, ViewKanbanTicket::class.java)
-        activityIntent.putExtra(Globals.BROADCAST_STORY_ID, storyId)
-        activityIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
-        context.startActivity(activityIntent)
+        val sharedPref = context.getSharedPreferences("UserInfo", AppCompatActivity.MODE_PRIVATE)
+        val userId = sharedPref.getString("id", "")!!
+        val db = Firebase.firestore
+        val storyDao = StoryDao(db)
+        val repository = StoryRepository(storyDao)
+        CoroutineScope(Dispatchers.IO).launch {
+            val stories = repository.getAllByUserId(userId)
+            for (story in stories) {
+                if (story.id == storyId) {
+                    activityIntent.putExtra("story_id", story)
+                    activityIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                    context.startActivity(activityIntent)
+                }
+            }
+        }
     }
 }
